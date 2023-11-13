9d83a610e8b102de493d0b46181c1e5e44a2f022
Deepak kumar <143509434+Deepak-kumar-2023@users.noreply.github.com>
2023-11-14 00:31:12 +0530

    my portfolio

---
+++ com/company/ex.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a ={2,1,3,4,5};
+
+       
+
+
+}}
